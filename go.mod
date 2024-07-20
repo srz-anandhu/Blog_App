7c4a023e480aabb0906beae992e4261dabe81ff4
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2024-07-20 11:51:14 +0530

    created 'CreateUser' function

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module blog
 
 go 1.22.4
 
-require github.com/lib/pq v1.10.9 // indirect
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
 	"database/sql"
+	"encoding/hex"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -30,10 +34,57 @@ func main() {
 	// close db connection before main function exit
 	defer Db.Close()
 	// connection checking
-	if err:=Db.Ping();err!=nil{
+	if err := Db.Ping(); err != nil {
 		log.Fatal(err)
 	}
-	 
+
 	fmt.Println("successfully connected to database....")
 
+	// User Creation
+	userId, err := CreateUser("aadhil", "strongpassword2")
+	if err != nil {
+		log.Printf("user creation failed due to: %s", err)
+	}
+	fmt.Printf("User created with ID:%d", userId)
+}
+
+func CreateUser(username, password string) (uint16, error) {
+	// Generate salt
+	salt, err := GenerateSalt(10)
+	if err != nil {
+		log.Printf("error generating salt: %v", err)
+	}
+
+	// Password Hashing
+	hashedPassword := HashPassword(password, salt)
+
+	var userId uint16
+	query := `INSERT INTO users(username,password,salt,created_at,updated_at,is_deleted)
+			VALUES ($1,$2,$3,$4,$5,$6) RETURNING id`
+
+	if err := Db.QueryRow(query, username, hashedPassword, salt, time.Now(), time.Now(), false).Scan(&userId); err != nil {
+		return 0, fmt.Errorf("error while executing query:%v ", err)
+	}
+
+	return userId, nil
+}
+
+// Generate a random salt of the given length
+func GenerateSalt(length uint8) (string, error) {
+	bytes := make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	saltString := hex.EncodeToString(bytes)
+	return saltString, nil
+}
+
+// Hashes the password with given salt (SHA-256)
+func HashPassword(password, salt string) string {
+	hash := sha256.New()
+	hash.Write([]byte(password + salt))
+	hashBytes := hash.Sum(nil)
+	hashedPass := hex.EncodeToString(hashBytes)
+	return hashedPass
 }
