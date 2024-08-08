4ef061aa55680887a4d7edf363c5e022edbbe403
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2024-08-08 09:29:09 +0530

    added get all users functionality

--- app/controller/user_controller.go
+++ app/controller/user_controller.go
@@ -22,6 +22,30 @@ func NewUserController() UserController {
 
 func (c *userControllerImpl) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 
+	var users []dto.UserResponse
+
+	user1 := dto.UserResponse{
+		ID:       2,
+		UserName: "bbbbb@gmail.com",
+	}
+	user2 := dto.UserResponse{
+		ID:       3,
+		UserName: "ccccc@gmail.com",
+	}
+
+	users = append(users, user1, user2)
+
+	jsonData, err := json.Marshal(users)
+	if err != nil {
+		log.Printf("error due to : %s ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(jsonData))
 }
 
 func (c *userControllerImpl) GetUser(w http.ResponseWriter, r *http.Request) {
