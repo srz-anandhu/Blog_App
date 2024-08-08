fb60da1430bd50cb7ac3b392d0ee71b961ecff26
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2024-08-08 09:41:53 +0530

    added get all authors functionality

--- app/controller/author_controller.go
+++ app/controller/author_controller.go
@@ -22,6 +22,30 @@ func NewAuthorController() AuthorController {
 
 func (c *authorControllerImpl) GetAllAuthors(w http.ResponseWriter, r *http.Request) {
 
+	var authors []dto.AuthorResponse
+
+	author1 := dto.AuthorResponse{
+		ID:   2,
+		Name: "Author Name 8888",
+	}
+	author2 := dto.AuthorResponse{
+		ID:   3,
+		Name: "Author Name 77777",
+	}
+
+	authors = append(authors, author1, author2)
+
+	jsonData, err := json.Marshal(authors)
+	if err != nil {
+		log.Printf("error due to : %s ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(jsonData))
 }
 
 func (c *authorControllerImpl) GetAuthor(w http.ResponseWriter, r *http.Request) {
